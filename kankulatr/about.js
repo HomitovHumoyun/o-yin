e33e60a0c022b69d302b133a58329aa7209e0407
777 <HomitovHumoyun>
2024-08-10 15:15:05 +0500

    first commit

---
+++ kankulatr/about.js
@@ -0,0 +1,12 @@
+function numCatch(a){
+    let inp1=document.getElementById('input1');
+inp1.value=inp1.value+a.value;
+}
+function barobar(){
+    let r=document.getElementById('input1');
+    r.value=eval(r.value);
+}
+function reset(){
+    let res=document.getElementById('input1');
+    res.value="";
+}
